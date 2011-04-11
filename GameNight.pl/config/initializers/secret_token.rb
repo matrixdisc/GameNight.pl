d333895645e7f144412838bd9f2b7d7f38ebe8f2
Mateusz Lukomski <matrix.disc@gmail.com>
2011-04-11 16:16:00 +0200

    Generated project in RubyMine

---
+++ GameNight.pl/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameNightPl::Application.config.secret_token = 'c14699b79aa2f8f5b8817d526bd8a616e20fb6cc87678d8ae77215c4ecc2e6cff73d6f33904ca3cd07a5ee37c63fc3b527f83d7233686734d0f0df993acb57eb'
