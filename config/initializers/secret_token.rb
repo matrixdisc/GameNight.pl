7068902debdc17ad6adc0f7b848a0261cb090031
Maciej <maciej@maciejkowalewski.com>
2011-04-13 20:06:12 +0200

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameNightPl::Application.config.secret_token = 'e6e6ec370a1148a3f46b0cd9c39a06ee15df8e35deda49f37302f0d07829a769fe7f12f1995f99d33f81e4670656106bd6d9a0ebb20c1195b5bf325b278a3f0e'
