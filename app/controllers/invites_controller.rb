ca579e208544ae705dc741762f7b940a200d4d03
Marcel Makowski <marcel@kwach.org>
2011-05-11 17:58:16 +0200

    gui enhancements, added unversioned files

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -5,10 +5,17 @@ class InvitesController < ApplicationController
 
   def accept
     invite = Invite.find(params[:id])
+    @user = invite.user_target
     invite.accepted_at=Time.now
     invite.is_accepted=true
     invite.save
-    redirect_to(:friends)
+    name = "unknown"
+    if invite.user_target.eql?(current_user)
+       name = invite.user.firstname
+    else
+       name = invite.user_target.firstname
+    end
+    redirect_to(:friends, :notice => "#{name} is now your friend!")
   end
 
   def destroy
