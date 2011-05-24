57a05bb81d220177bfca88ff6018a5da8267302b
Mateusz Lukomski <matrix.disc@gmail.com>
2011-05-24 13:31:29 +0200

    Showing players invited to a gamenight and players/player slots

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,22 @@
 
 @matrix = User.create(:username => "ma_trix", :email => "matrix.disc@gmail.com", :first_name => "Mateusz", :last_name => 'Łukomski', :location => 'Poznań', :password => "pass", :password_confirmation => "pass")
 if @matrix.save
-  puts "matrix saved"
+  puts "User ma_trix created and saved"
 else
   puts "matrix not saved"
 end
 @marcel = User.create(:username => "marcel", :email => "marcel@kwach.org", :first_name => "Marcel", :last_name => 'Makowski', :location => 'Poznań', :password => "pass", :password_confirmation => "pass")
 if @marcel.save
-  puts "marcel saved"
+  puts "User marcel created and saved"
 else
   puts "marcel not saved"
 end
 @friendship = Invite.create(:user => @matrix, :user_target => @marcel, :message => "Friends?", :is_accepted => true)
-@friendship.save
+if @friendship.save
+  puts "ma_trix and marcel are friends now"
+else
+  puts "friendship not saved"
+end
 @gamenight = Gamenight.create(:user => @matrix, :name => "First gamenight", :description => "First gamenight ever", :location => "Poznań", :start_time => "15:00 10-05-2011", :end_time => "24:00 10-05-2011", :player_slots => 5)
 #@gamenight = @matrix.gamenights.create(:name => "First gamenight", :description => "First gamenight ever", :location => "Poznań", :start_time => "15:00 10-05-2011", :end_time => "24:00 10-05-2011", :player_slots => 5)
 if @gamenight.save
