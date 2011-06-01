5bed6a2ce240b9e8d5ea1ed214a6367394a1eb4e
Marcel Makowski <marcel@kwach.org>
2011-06-02 00:42:09 +0200

    massive enhancements to the dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   def index
     @user = current_user
     @gamenights = current_user.gamenights + Gamenight.find_all_by_host_id(current_user.id)
+    @notifications = Notification.order("created_at desc").find_all_by_recipient_id(current_user.id)
   end
 
 end
