dbfafe1e1e34efec412b46295b64fbaf550bc3d8
Maciej <maciej@maciejkowalewski.com>
2011-06-05 17:16:47 +0200

    Nice gamenight show view

--- app/models/gamenight.rb
+++ app/models/gamenight.rb
@@ -26,4 +26,24 @@ class Gamenight < ActiveRecord::Base
   belongs_to :game
   has_many :gamenight_invitations
   has_many :users , :through => :gamenight_invitations
+
+  def duration
+    if not self.start_time or not self.end_time then
+      return "unknown"
+    end
+
+    if self.start_time.to_date == self.end_time.to_date then
+      start_time_string = self.start_time.strftime("%A, %d %b %Y %l:%M%p")
+      end_time_string = self.end_time.strftime("%l:%M%p")
+      return "#{start_time_string} - #{end_time_string}"
+    else
+      start_time_string = self.start_time.strftime("%A, %d %b %Y %l:%M%p")
+      end_time_string = self.end_time.strftime("%A, %d %b %Y %l:%M%p")
+      return "#{start_time_string} - #{end_time_string}"
+    end
+  end
+
+  def can_invite_more_people?
+    self.players_slots.nil? || (self.players_slots > self.gamenight_invitations.size + 1)
+  end
 end
