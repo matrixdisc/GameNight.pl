a6f5624959e6cd1445a5fc26d690aa63e974de84
Maciej <maciej@maciejkowalewski.com>
2011-06-10 21:58:21 +0200

    Dropdown for token input displays covers

--- app/models/game.rb
+++ app/models/game.rb
@@ -12,4 +12,13 @@
 
 class Game < ActiveRecord::Base
   mount_uploader :cover, GameCoverUploader
+
+  def thumb_url
+    self.cover.thumb.url
+  end
+
+
+  def to_json
+    return self.attributes.merge(thumb_url: self.thumb_url)
+  end
 end
